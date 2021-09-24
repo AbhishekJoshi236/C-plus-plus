47dd583d9ed24f96c73baffe0adc9265568f435c
AbhishekJoshi236 <90817430+AbhishekJoshi236@users.noreply.github.com>
2021-09-24 15:47:39 +0200

    Add files via upload

---
+++ Practical 2/PRIME/code.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+int n,flag=0;
+cout<<"Enter number: ";
+cin>>n;
+if(n==1 || n==0)
+cout<<"Not a prime number.";
+else{
+for(int i=2;i<n/2;i++)
+{
+	if(n%i==0)
+	{
+	cout<<"Not a prime number.";
+	flag=1;
+	break;
+	}
+}
+
+if(flag==0)
+	cout<<"Its is a prime number."<<endl;
+}
+
+return 0;
+}
