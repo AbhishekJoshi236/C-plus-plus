740d3c460f59e2bda1e50aade8e62162eda2ba87
AbhishekJoshi236 <90817430+AbhishekJoshi236@users.noreply.github.com>
2021-10-08 21:13:40 +0530

    Add files via upload

---
+++ Practical 5/task1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char c='a';
+	short s=-1;
+	int i=330014;
+	bool b=true;
+	long l=23894634;
+	float f=1.92f;
+	long double ld=78.8956;
+	wchar_t wc=L'a';
+	cout<<"Character variable	:"<<c<<endl;
+	cout<<"Short variable		:"<<s<<endl;
+	cout<<"Integer variable		:"<<i<<endl;
+	cout<<"Bool variable		:"<<b<<endl;
+	cout<<"Long variable		:"<<l<<endl;
+	cout<<"Float variable		:"<<f<<endl;
+	cout<<"Long Double variable	:"<<ld<<endl;
+	cout<<"Wide Character variable	:"<<wc<<endl;
+return 0;
+}
