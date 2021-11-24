0f7646e0f0aa27ad61a66cf4eecd54c288ea2ca5
AbhishekJoshi236 <90817430+AbhishekJoshi236@users.noreply.github.com>
2021-11-24 19:09:32 +0530

    Update task9.3.cpp

--- Practical 9/task9.3.cpp
+++ Practical 9/task9.3.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
 using namespace std;
-class Static{
-    public:
-        static int a;
-        Static(int b){
-            a++;
-        }
+
+class Count
+{
+   public:
+      static int a;
+      Count(int b)
+      {
+          a++;
+      }
 };
-int Static::a=0;
-int main(){
-    Static s1(98),s2(67),s3(50),s4(90),s5(20);
-    cout<<"Total number of calls for a members function: "<<Static::a<<endl;
+
+int Count::a=0;
+
+int main()
+{
+    Count s(5),t(6),u(10);
+    cout<<"Total number of calls for a member function: "<<Count::a<<endl;
 }
