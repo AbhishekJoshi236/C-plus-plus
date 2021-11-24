8252a4d684dddb57811546fe42faed77878c2331
AbhishekJoshi236 <90817430+AbhishekJoshi236@users.noreply.github.com>
2021-11-24 19:10:46 +0530

    Update task9.4.cpp

--- Practical 9/task9.4.cpp
+++ Practical 9/task9.4.cpp
@@ -1,29 +1,33 @@
 #include<iostream>
 using namespace std;
-class Student{
+
+class Student
+{
     string name;
     int age;
     int marks;
     public:
-        Student(string s,int a,int m){
+        Student(string s,int a,int m)
+        {
             name=s;
             age=a;
             marks=m;
         }
         friend int Add(Student m);
 };
+
 int sum=0;
-int c;
-int Add(Student m){
-    c++;
+
+int Add(Student m)
+{
     sum=sum+m.marks;
 };
-int main(){
-    Student s1("Abhishek",18,50),s2("Manish",19,50);
-    Student s3("Ram",21,50),s4("Deepak",20,50);
-    Add(s1);
-    Add(s2);
-    Add(s3);
-    Add(s4);
-    cout<<"Total sum of marks of "<<c<<" students is: "<<sum<<endl;
+
+int main()
+{
+    Student s("Albert",18,35),t("Bony",19,20),u("Cia",21,19);
+    Add(s);
+    Add(t);
+    Add(u);
+    cout<<"Total sum of marks of 3 students is: "<<sum<<endl;
 }
