68d22b78d12a71dd208a53461004bd8d437690c6
AbhishekJoshi236 <90817430+AbhishekJoshi236@users.noreply.github.com>
2021-12-20 17:57:03 +0530

    Update task2.cpp

--- Practical 6/task2.cpp
+++ Practical 6/task2.cpp
@@ -1,18 +1,17 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
+
 int main()
-{
-	string s,a;
-	cout<<"Enter string:";
-	getline(cin,s);
+{ 
+  string s;
+  cout<<"Enter the string:";
+  getline(cin,s);
 	
-	for(int i=s.size()-1;i>=0;i--)
-		a.push_back(s[i]);
-		
-	if(a==s)
-		cout<<"is palindrome";
-	else
-		cout<<"not palindrome";
-return 0;
+  string::reverse_iterator i=s.rbegin();
+  if(equal(s.begin(),s.end(),i))
+      cout<<"Yes it is palindrome"<<endl;
+ else
+  cout<<"No it is not a palindrome"<<endl;
+	
+ return 0;
 }
