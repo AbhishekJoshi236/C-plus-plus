154aa5075f41a4a8ad9b81cc44a3c70353b8bf5c
AbhishekJoshi236 <90817430+AbhishekJoshi236@users.noreply.github.com>
2021-12-23 18:52:24 +0530

    Create Task_11.2.cpp

---
+++ Practical 11/Task_11.2.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+class mammals
+{
+    public:
+    void print_mammals()
+    {
+        cout<<"I am mammals.  \n";
+    }
+
+};
+
+class marine_animals
+{
+    public:
+    void print_marine_mammals()
+    {
+        cout<<"I am marine mammals.  \n";
+    }
+
+};
+
+class blue_whale: public mammals, public marine_animals
+{
+    public:
+    void print_both()
+    {
+        cout<<"I belongs to both the categories: Mammals as well as Marine Animals.  \n";
+    }
+};
+
+int main()
+{
+    mammals obj1;
+    marine_animals obj2;
+    blue_whale obj3;
+    cout<<endl;
+  
+    obj1.print_mammals();           //1
+    obj2.print_marine_mammals();    //2
+    obj3.print_both();              //3     //4
+
+    obj3.print_marine_mammals();            //4
+    obj3.print_mammals();                   //4
+    return 0;
+}
