4d4b1d6066828c912f50427c1dd645479b85a9f2
AbhishekJoshi236 <90817430+AbhishekJoshi236@users.noreply.github.com>
2021-12-23 19:07:04 +0530

    Create Task_12.2(a).cpp

---
+++ Practical 12/Task_12.2(a).cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+/*
+Constructor Calling -> From Derived class to Base Class.
+Constructor Execution -> From Base class to Derived Class.
+*/
+
+class A
+{
+    public:
+    A()                                             // Secondly calling class B constructor
+    {
+        cout<<"\n Class A Constructor execution....\n\n";
+    }
+
+};
+
+class B: public A
+{
+    public:
+    B()                                             // Firstly calling class B constructor
+    {
+        cout<<"\n Class B Constructor execution....\n\n"; 
+    }
+
+};
+
+int main()
+{
+    B obj;
+    return 0;
+}
