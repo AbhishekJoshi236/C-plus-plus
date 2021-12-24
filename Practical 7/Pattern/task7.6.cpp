bdcd04fd0a5ccbe52654fb002c787c7bd846e5bf
AbhishekJoshi236 <90817430+AbhishekJoshi236@users.noreply.github.com>
2021-12-24 18:50:11 +0530

    Update task7.6.cpp

--- Practical 7/Pattern/task7.6.cpp
+++ Practical 7/Pattern/task7.6.cpp
@@ -1,24 +1,23 @@
 #include<iostream>
 using namespace std;
-
 int main()
-{
-	for(int i=0;i<5;i++)
-	{
-		int c=i;
-		while(c!=0)
-		{
-			cout<<" ";
-			c--;
-		}	
-		
-		for(int j=i;j<5;j++)
-		{
-			cout<<"*";
-		}
-		
-		cout<<"\n";
-	}
-
+{ 
+  char a[4][4];
+  cout<<"Pattern Matrix:\n";
+	
+  for(int i=0;i<4;i++)
+    for(int j=0;j<4;j++)
+       { 
+	    if(i<=j)
+             a[i][j]='*';
+            else
+             a[i][j]=' ';
+       }
+    for(int i=0;i<4;i++)
+    {   
+	for(int j=0;j<4;j++)
+            cout<<a[i][j];
+     cout<<endl;
+    }
 return 0;
 }
